5f38e4fb2212e4ac98ce4aa891b374657044659e
Andrew Holzer <ajholzer@gmail.com>
2024-04-18 18:55:40 -0700

    Initial commit

---
+++ components/pmod_maxsonar/pmod_maxsonar.cpp
@@ -0,0 +1,113 @@
+#include "esphome/core/log.h"
+#include "pmod_maxsonar.h"
+
+namespace esphome {
+namespace pmod_maxsonar {
+
+static const char *TAG = "pmod_maxsonar.sensor";
+
+void PmodMaxSonar::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up PmodMaxSonar '%s'...", this->name_.c_str());
+
+  if (this->enable_rx_pin_ != nullptr) {
+    this->enable_rx_pin_->setup();
+    this->enable_rx_pin_->pin_mode(gpio::FLAG_OUTPUT);
+    this->enable_rx_pin_->digital_write(false);
+  }
+
+}
+
+void PmodMaxSonar::loop() {}
+
+void PmodMaxSonar::update() {
+  const int DATA_SIZE = 5;
+  uint8_t read_data[DATA_SIZE];
+  const int TIMEOUT_MAX = 50;
+  int timeoutCount = 0;
+  uint16_t data = 0;
+  char buffer[3];
+
+  this->enable_rx_pin_->digital_write(true);
+  delay(1);
+  this->enable_rx_pin_->digital_write(false);
+
+  while(!this->available() && timeoutCount++ < TIMEOUT_MAX) {
+    delay(1);
+  }
+
+  if (this->available()) {
+    char first = this->read();
+
+    if (first != 'R') {
+      ESP_LOGE(TAG, "failed first byte");
+
+      return;
+    }
+
+    for (auto i = 0; i < 3; i++) {
+      while(!this->available());
+
+      buffer[i] = this->read();
+    }
+
+    while(!this->available());
+
+    if (this->read() != '\r') { // ascii 13
+      ESP_LOGE(TAG, "return byte failed");
+
+      data = 0;
+    }
+
+    data = atoi(buffer);
+  }
+
+  if (timeoutCount >= TIMEOUT_MAX) {
+    data = -1;
+  }
+
+  ESP_LOGD(TAG, "got maxsonar measurement: %d", data);
+
+  this->publish_state(data);
+}
+
+void PmodMaxSonar::read_data() {
+
+  // this->read_array(read_data, DATA_SIZE);
+  // // this->flush();
+  // this->enable_rx_pin_->digital_write(false);
+
+  // for (auto i = 0; i < DATA_SIZE; i++) {
+  //   ESP_LOGI(TAG, "data\t|\tindex: '%d'\t|\tvalue: '%d'", i, read_data[i]);
+  // }
+
+  // if (read_data[4] != 'R') {
+  //   ESP_LOGW(TAG, "Got faulty reading from MAXSONAR, first char isn't 'R', instead got char id '%d'", read_data[4]);
+  //   this->publish_state(NAN);
+  //   this->status_set_warning();
+  //   return;
+  // }
+
+  //  if (read_data[0] != '\r') {
+  //   ESP_LOGW(TAG, "Got faulty reading from MAXSONAR, last char isn't '\\r', instead got char id '%d'", read_data[0]);
+  //   this->publish_state(NAN);
+  //   this->status_set_warning();
+  //   return;
+  //  }
+
+  // char frame[3];
+  // for (int i = 0; i < 3;i++) {
+  //   frame[i] = read_data[3-i];
+  // }
+
+  // uint16_t data = atoi(frame);
+}
+
+void PmodMaxSonar::dump_config() {
+  ESP_LOGCONFIG(TAG, "PmodMaxSonar '%s'", this->name_.c_str());
+  if (this->enable_rx_pin_ != nullptr) {
+    LOG_PIN("  Enable RX Pin: ", this->enable_rx_pin_);
+  }
+}
+
+}  // namespace pmod_maxsonar
+}  // namespace esphome
