48ee2f65f858a7024a0442bb4413643c578a240d
Andrew Holzer <ajholzer@gmail.com>
2024-04-18 18:55:40 -0700

    Initial commit

---
+++ components/random_uart_sensor/random_uart_sensor.cpp
@@ -0,0 +1,27 @@
+#include "esphome/core/log.h"
+#include "random_uart_sensor.h"
+
+namespace esphome {
+namespace random_uart_sensor {
+
+static const char *TAG = "random_uart_sensor.sensor";
+
+void RandomUARTSensor::setup() {
+
+
+}
+
+void RandomUARTSensor::update() {
+    this->publish_state(42);
+}
+
+void RandomUARTSensor::loop() {
+
+}
+
+void RandomUARTSensor::dump_config(){
+    ESP_LOGCONFIG(TAG, "Random UART sensor");
+}
+
+}  // namespace random_UART_sensor
+}  // namespace esphome
